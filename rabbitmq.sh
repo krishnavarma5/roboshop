00f116b320104c1fa82cdfe1705ba48ba6ece655
krishnavarma5 <krishnavarma5penmetsa@gmail.com>
2023-12-27 17:32:24 +1030

    updated file

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -29,3 +29,31 @@ then
 else
    echo "you are root user"
 fi #fi means reverse of if, indicating condition end
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>> $LOGFILE
+
+VALIDATE $? "Downloading erlang script"
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>> $LOGFILE
+
+VALIDATE $? "Downloading rabbitmq script"
+
+dnf install rabbitmq-server -y &>> $LOGFILE
+
+VALIDATE $? "Installing rabbitmq server" 
+
+systemctl enable rabbitmq-server &>> $LOGFILE
+
+VALIDATE $? "enabling rabbitmq server"
+
+systemctl start rabbitmq-server &>> $LOGFILE
+
+VALIDATE $? "starting rabbitmq server"
+
+rabbitmqctl add_user roboshop roboshop123 &>> $LOGFILE
+
+VALIDATE $? "creating user"
+
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> $LOGFILE
+
+VALIDATE $? "setting permissions"
